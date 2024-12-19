e7eec057b35fe173235d7050b1270015ef5a48d8
Odinachi David <odinachidavid@yahoo.com>
2024-12-19 15:07:53 +0100

    Initial ui done

--- lib/features/home_screen/view/home_screen.dart
+++ lib/features/home_screen/view/home_screen.dart
@@ -1,10 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/shared/constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Hello Arian,",
+                style: baseStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                "Discover the weather",
+                style: baseStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const WeatherCard(
+                location: "Lagos",
+                temp: 33,
+                weatherType: "Rainy",
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class WeatherCard extends StatelessWidget {
+  const WeatherCard(
+      {super.key,
+      required this.location,
+      required this.temp,
+      required this.weatherType});
+
+  final String location;
+  final String weatherType;
+  final num temp;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: AppColors.purple,
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Current location",
+                style: baseStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.white,
+                ),
+              ),
+              Text(
+                location,
+                style: baseStyle.copyWith(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.white,
+                ),
+              ),
+              Text(
+                weatherType,
+                style: baseStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.white,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/shared/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const baseStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+class AppColors {
+  static const purple = Color(0xff6151C3);
+  static const white = Colors.white;
+}
