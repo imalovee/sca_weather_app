592d14b06fb53d82fc6868161ad526ee71b78b9a
Odinachi David <odinachidavid@yahoo.com>
2024-12-19 14:53:46 +0100

    Initial state

---
+++ lib/shared/routes/route_strings.dart
@@ -0,0 +1,5 @@
+class AppRouteStrings{
+  static const String base = '/';
+  static const String homeScreen = '/home_screen';
+  static const String weatherDetailsScreen = '/weather_details_screen';
+}
---
+++ lib/shared/routes/router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weatherapp/features/home_screen/view/home_screen.dart';
+import 'package:weatherapp/features/weather_details/weather_details_screen.dart';
+import 'package:weatherapp/shared/routes/route_strings.dart';
+
+class AppRouter {
+  static final navKey = GlobalKey<NavigatorState>();
+
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRouteStrings.base:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+      case AppRouteStrings.homeScreen:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+      case AppRouteStrings.weatherDetailsScreen:
+        return CupertinoPageRoute(builder: (_) => const WeatherDetailsScreen());
+
+      default:
+        return CupertinoPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
+}
